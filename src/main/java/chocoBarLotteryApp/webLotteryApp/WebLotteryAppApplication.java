57147c078f44a9545948858acefed0403246c9c1
Bartosz Tyra <57729511+fernil@users.noreply.github.com>
2023-04-19 21:11:54 +0200

    commit

---
+++ src/main/java/chocoBarLotteryApp/webLotteryApp/WebLotteryAppApplication.java
@@ -0,0 +1,13 @@
+package chocoBarLotteryApp.webLotteryApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebLotteryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebLotteryAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chocoBarLotteryApp/webLotteryApp/WebLotteryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package chocoBarLotteryApp.webLotteryApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebLotteryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
